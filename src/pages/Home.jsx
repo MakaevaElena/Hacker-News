9755563f3d0baa700aa14547830ce2451539c8df
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-11-17 17:59:19 +0700

    createAsyncThunk fetchStories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import NewsBlock from '../components/NewsBlock';
-import { BASE_API_URL } from '../const.jsx';
+// import { BASE_API_URL } from '../const.jsx';
 import { Button } from 'antd';
-import { fetchNewsIds } from '../redux/slices/newsSlice';
 import { useSelector, useDispatch } from 'react-redux';
-import { setIsLoading, setNews } from '../redux/slices/newsSlice';
+import { setIsLoading, fetchNewsIds, fetchStories } from '../redux/slices/newsSlice';
 
 const Home = (props) => {
   const dispatch = useDispatch();
@@ -12,47 +11,24 @@ const Home = (props) => {
   const newsIds = useSelector((state) => state.news.newsIds);
   const news = useSelector((state) => state.news.news);
   const isLoading = useSelector((state) => state.news.isLoading);
-  console.log(newsIds);
+  // console.log(newsIds);
   // console.log(news);
-  // console.log(isLoading);
+  console.log(isLoading);
 
   React.useEffect(() => {
     dispatch(fetchNewsIds());
-    // fetchNewsIds();
-    fetchStories();
+    dispatch(fetchStories(newsIds));
     // setInterval(fetchStories, 10 * 60 * 1000);
-    setIsLoading(false);
-  }, [dispatch]);
-
-  // function fetchNewsIds() {
-  //   fetch(`${BASE_API_URL}/newstories.json?print=pretty`)
-  //     .then((res) => res.json())
-  //     .then((arr) => {
-  //       const newsIds = arr.slice(0, 100);
-  //       dispatch(setNewsIds(newsIds));
-  //       // console.log('fetchNewsIds', newsIds);
-  //     });
-  // }
-
-  function fetchStories() {
-    let stories = [];
-    newsIds.forEach((storyId) => {
-      fetch(`${BASE_API_URL}/item/${storyId}.json?print=pretty`)
-        .then((res) => res.json())
-        .then((story) => {
-          stories = [...stories, story];
-          dispatch(setNews(stories));
-        });
-    });
-  }
+    dispatch(setIsLoading(false));
+  }, []);
 
   return (
     <div className="home">
-      <Button type="ghost" onClick={() => fetchStories()}>
+      <Button type="ghost" onClick={() => dispatch(fetchStories(newsIds))}>
         Обновить новости
       </Button>
       <div className="content__items">
-        {isLoading && <p>Загрузка</p>}
+        {isLoading ? <p>Загрузка</p> : ''}
         {news.map((obj) => (
           <NewsBlock key={obj.id} {...obj} /> //передали в пропсы все поля объекта
         ))}
--- src/redux/slices/newsSlice.jsx
+++ src/redux/slices/newsSlice.jsx
@@ -2,16 +2,6 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { BASE_API_URL } from '../../const';
 
 //////////////////////
-// function getNewsIds() {
-//   fetch(`${BASE_API_URL}/newstories.json?print=pretty`)
-//     .then((res) => res.json())
-//     .then((arr) => arr.slice(0, 100))
-//     // .then((arr) => console.log(arr));
-//     .then((arr) => {
-//       console.log(arr);
-//       return arr;
-//     });
-// }
 
 // при запросе fetch нужно использовать async-await вместо .then
 async function getNewsIds() {
@@ -21,6 +11,24 @@ async function getNewsIds() {
 }
 
 export const fetchNewsIds = createAsyncThunk('news/fetchNewsIds', async () => getNewsIds());
+
+// async await не работает с forEach - замена на for -of
+async function getStories(storyIds) {
+  let stories = [];
+  for (let storyId of storyIds) {
+    const response = await fetch(`${BASE_API_URL}/item/${storyId}.json?print=pretty`);
+    const story = await response.json();
+    stories = [...stories, story];
+    console.log(stories);
+    // очень долго загружаются истории 50 секунд и только при нажатиии кнопки
+  }
+  console.log(stories);
+  return stories;
+}
+export const fetchStories = createAsyncThunk('news/fetchStory', async (storyIds) =>
+  getStories(storyIds),
+);
+
 /////////////////////
 const initialState = {
   isLoading: true,
@@ -35,12 +43,10 @@ export const newsSlice = createSlice({
     setIsLoading: (state, action) => {
       state.isLoading = action.payload;
     },
-    // setNewsIds: (state, action) => {
-    //   state.newsIds = action.payload;
+
+    // setNews: (state, action) => {
+    //   state.news = action.payload;
     // },
-    setNews: (state, action) => {
-      state.news = action.payload;
-    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchNewsIds.pending, (state, action) => {
@@ -50,9 +56,16 @@ export const newsSlice = createSlice({
       state.isLoading = 'false';
       state.newsIds = action.payload;
     });
+    builder.addCase(fetchStories.pending, (state, action) => {
+      state.isLoading = 'true';
+    });
+    builder.addCase(fetchStories.fulfilled, (state, action) => {
+      state.news = action.payload;
+      state.isLoading = 'false';
+    });
   },
 });
 
-export const { setIsLoading, setNewsIds, setNews } = newsSlice.actions;
+export const { setIsLoading, setNews } = newsSlice.actions;
 
 export default newsSlice.reducer;
